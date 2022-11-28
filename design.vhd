24ed4fa6def6d3de3caf4cbc875fc0020b86470c
Jesus Avila <jesus.avila.05@gmail.com>
2022-11-28 11:38:08 -0300

    creado design.vhd

---
+++ design.vhd
@@ -0,0 +1,26 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity receptor_control_remoto is 
+    port (
+        rst :           in std_logic;
+        infrarrojo :    in std_logic;
+        hab :           in std_logic;
+        clk :           in std_logic;
+
+        valido :        out std_logic;
+        dir     :       out std_logic_vector (7 downto 0);
+        cmd             out std_logic_vector (7 downto 0););
+
+end receptor_control_remoto;
+
+
+architecture solucion of receptor_control_remoto is 
+
+begin
+    
+
+end solucion;
+
+
+
