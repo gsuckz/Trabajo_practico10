4e11c549760bcc73c61342853d9c409fcb785ea8
lucas <lucasg.exe.1998@gmail.com>
2022-11-30 13:08:10 -0300

    Modificado make y testbench

--- Makefile
+++ Makefile
@@ -1,9 +1,23 @@
+ops = --std=08
+arch_cf = work-onj08.cf
+nombre = design
+tb = testbench
+arch_wav = $(nombre).ghw
+wav_ops = --assert-level-none --wave=$(arch_wav)
 all : receptor_control_remoto
-
+.PHONY: all test wav
 receptor_control_remoto : work-obj08.cf 
 	ghdl -m --std=08 receptor_control_remoto_tb
 	ghdl -r --std=08 receptor_control_remoto_tb
 
 
-work-obj08.cf: *.vhd
-	ghdl -i --std=08 *.vhd
+work-obj08.cf: testbench.vhd
+	ghdl -i --std=08 testbench.vhd
+
+wav: $(arch_wav)
+		gtkwave -f $(arch_wav)
+	 $(arch_wav): $(arch_cf)
+	 ghdl -m $(ops) $(tb)
+	 ghdl -r $(ops) $(tb) $(wav_ops)
+	 $(arch_cf): testbench.vhd
+	 ghdl -i $(ops) testbench.vhd
--- testbench.vhd
+++ testbench.vhd
@@ -1,31 +1,50 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+
 entity receptor_control_remoto_tb is
 end receptor_control_remoto_tb;
 
 
+
 architecture tb of receptor_control_remoto_tb is
-component receptor_control_remoto is 
-    port (
-        rst :           in std_logic;
-        infrarrojo :    in std_logic;
-        hab :           in std_logic;
-        clk :           in std_logic;
-
-        valido :        out std_logic;
-        dir     :       out std_logic_vector (7 downto 0);
-        cmd             out std_logic_vector (7 downto 0););
-
+    component receptor_control_remoto is 
+        port (
+        rst         :    in std_logic;
+        infrarrojo  :    in std_logic;
+        hab         :    in std_logic;
+        clk         :    in std_logic;
+        valido      :    out std_logic;
+        dir         :    out std_logic_vector (7 downto 0);
+        cmd         :    out std_logic_vector (7 downto 0));
     end component;
+    --Constantes
+    constant clk_H      :    time := 187.5 us;
+    constant clk_L      :    time := 187.5 us;
+
+    --Señales
     signal rst_in, infrarrojo_in, hab_in, clk_in, valido_out : std_logic;
     signal dir_out, cmd_out : std_logic_vector (7 downto 0);
 
 begin 
-    DUT : receptor_control_remoto port map (rst=>rst_in, infrarrojo=>infrarrojo_in, hab=>hab_in, clk=>clk_in, 
-                                            valido=>valido_out, dir=>dir_out, cmd=>cmd_out);
 
-   
+    DUT : receptor_control_remoto port map (
+        rst         =>    rst_in, 
+        infrarrojo  =>    infrarrojo_in, 
+        hab         =>    hab_in, 
+        clk         =>    clk_in,
+
+        valido      =>    valido_out, 
+        dir         =>    dir_out, 
+        cmd         =>    cmd_out);
+    
+clk_signal: process
+begin
+    clk_in <= '0';
+    wait for clk_L;
+    clk_in <= '1';
+    wait for clk_H;
+end process;
 
 
 end tb;
